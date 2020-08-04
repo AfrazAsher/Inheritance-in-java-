ca720b99f67b8ed939bda871cb5af4d6a0587ced
Afraz Asher Asrar <65652768+AfrazAsher@users.noreply.github.com>
2020-08-05 03:59:12 +0500

    Update DogTest.java

--- Lab-6/DogTest.java
+++ Lab-6/DogTest.java
@@ -1,7 +1,3 @@
-/*
-Name:-Afraz Asher Asrar
-Roll Number:- 303-BSCS-19(E1)
-*/
 public class DogTest
 {
 public static void main(String[] arg)
@@ -9,8 +5,8 @@ public static void main(String[] arg)
 	Dog D1 = new Dog("Dog");
 	System.out.println(D1.getName());
 	D1.Speak();
-	Labrador D2 = new Labrador("Brown");
-	System.out.println(D2.getColor()+D2.avgBreedWeight());
+	Labrador D2 = new Labrador("Labrador","Brown");
+	System.out.println(D2.Name+ "," +D2.getColor()+ "," +D2.avgBreedWeight());
 	D2.Speak();
 	Yorkshire D3 = new Yorkshire("Yorkshire");
 	System.out.println(D3.getName());
