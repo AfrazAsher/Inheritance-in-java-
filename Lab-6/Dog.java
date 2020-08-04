581224ae4f62ccad251f47a57c07f9c7c93ffd2d
Afraz Asher Asrar <65652768+AfrazAsher@users.noreply.github.com>
2020-08-05 03:56:27 +0500

    Update Dog.java

--- Lab-6/Dog.java
+++ Lab-6/Dog.java
@@ -1,21 +1,14 @@
-/*
-Name:-Afraz Asher Asrar
-Roll Number:- 303-BSCS-19(E1)
-*/
 public class Dog
 {
   protected String Name;
-  // Setter method
   public void setName(String Name)
   {
 	  this.Name=Name;
   }
-  // Getter method
   public String getName()
   {
 	  return Name;
   }
-  // Dog constructor
   public Dog(String Name)
   {
 	  this.Name=Name;
