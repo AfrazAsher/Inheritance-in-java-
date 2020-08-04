28665545739eae3a8b7c4d10b441c29b8509183d
Afraz Asher Asrar <65652768+AfrazAsher@users.noreply.github.com>
2020-08-05 03:57:28 +0500

    Update Yorkshire.java

--- Lab-6/Yorkshire.java
+++ Lab-6/Yorkshire.java
@@ -1,20 +1,13 @@
-/*
-Name:-Afraz Asher Asrar
-Roll Number:- 303-BSCS-19(E1)
-*/
 public class Yorkshire extends Dog
 {
-  // Setter methods
   public void setName(String Name)
   {
 	  this.Name=Name;
   }
-  // Getter methods
   public String getName()
   {
 	  return Name;
   }
-  // Yorkshire constructor
   public Yorkshire(String Name)
   {   
       super(Name);
@@ -22,8 +15,6 @@ public class Yorkshire extends Dog
   // overiding the "Speak" method here
   public void Speak()
   {   
-      // @override
       super.Speak();
-	  System.out.println("Woof");
   }
 }
